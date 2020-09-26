6b519c18c8aa23606478ada9d995b0ce93f31c18
nexbook <60404451+nexbook@users.noreply.github.com>
2020-09-25 21:33:40 -0600

    perfomance issues fixed

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,8 +4,8 @@ import './card.styles.css';
 
 export const Card = props => (
     <div className="card-container">
-        <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}/>
-        <h3> {props.monster.name} </h3>
+        <img className='monster-img' alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2`}/>
+        <p className='monster-name'> {props.monster.name} </p>
         <p> {props.monster.email} </p>
     </div>
 )
--- src/components/searchbox/searchbox.component.jsx
+++ src/components/searchbox/searchbox.component.jsx
@@ -4,6 +4,7 @@ import './searchbox.styles.css';
 
 export const SearchBox = ({placeholder, handleChange})=> (
     <input 
+    aria-label='Search Monsters'
     className="search"
     type="search" 
     placeholder='search monsters' 
