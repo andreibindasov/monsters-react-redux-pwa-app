07843f5a3869d52970c562dce9fba7f2d7238f7d
nexbook <60404451+nexbook@users.noreply.github.com>
2020-09-25 20:48:54 -0600

    Updates

---
+++ precache-manifest.274421c2a122214a6f13d98ab3d871eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e3170ca157877b57e6b3e4266ad342",
+    "url": "/monsters-react-redux-pwa-app/index.html"
+  },
+  {
+    "revision": "e5e4715b089a50b85436",
+    "url": "/monsters-react-redux-pwa-app/static/css/main.d5ab9c24.chunk.css"
+  },
+  {
+    "revision": "27cb46c16417509f91e5",
+    "url": "/monsters-react-redux-pwa-app/static/js/2.427d4d40.chunk.js"
+  },
+  {
+    "revision": "65f61a1b574cbfdae1aa1e0f7a13f7ca",
+    "url": "/monsters-react-redux-pwa-app/static/js/2.427d4d40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5e4715b089a50b85436",
+    "url": "/monsters-react-redux-pwa-app/static/js/main.1a8d057b.chunk.js"
+  },
+  {
+    "revision": "5ae762df6e514e248209",
+    "url": "/monsters-react-redux-pwa-app/static/js/runtime-main.2b06625d.js"
+  }
+]);
